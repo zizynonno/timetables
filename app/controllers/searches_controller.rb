8d658ce7946aa8815827e3b92af15d3e35ecc201
zizynonno <zizynonno@gmail.com>
2020-06-09 22:08:14 +0900

    教師名、科目名で検索可能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,18 +1,12 @@
 class SearchesController < ApplicationController
   def index
-    @subject_search_params = subject_search_params
-    @teacher_search_params = teacher_search_params
-    @searches = Subject.search(@subject_search_params)
-    @searches = Subject.joins(:teacher).search(@teacher_search_params)
+    @search_params = search_params
+    @searches = Subject.joins(:teacher).search(@search_params)
   end
 
   private
 
-  def subject_search_params
-    params.fetch(:search, {}).permit(:title)
-  end
-
-  def teacher_search_params
-    params.fetch(:search, {}).permit(:name)
+  def search_params
+    params.fetch(:search, {}).permit(:title,:name)
   end
 end
